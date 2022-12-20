ae8e22e6ee38e4c2584de334a276d4dd61593523
Tech-sis <abilawonadeola@gmail.com>
2022-12-20 13:09:39 -0500

    fix syntax errors

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -4,13 +4,12 @@
 
 def pascal_triangle(n):
     """ Function to create triangle"""
-    if (n <= 0) {
-		return [];
-	}
-    
+    if (n <= 0):
+        return []
+
     for line in range(1, n+1):
         num = 1
         for i in range(1, line+1):
-            print num,
-            num = num * (line - i) / i
-        print "\n"
+            print(num, end=" ")
+            num = int(num * (line - i) / i)
+        print("")
